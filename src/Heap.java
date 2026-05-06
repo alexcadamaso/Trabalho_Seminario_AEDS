f7aa068f46907c249c2f38d2089027efd5480507
Ritaa-Marie <ritamariecajuru@gmail.com>
2026-05-06 07:50:49 -0300

    MaxHeap implementado.

--- src/Heap.java
+++ src/Heap.java
@@ -1,20 +1,68 @@
 public class Heap {
     private Heap(){}
 
-    /*public static swap(float[] vetor, int a, int b){
-        if(a == b){
-            return null;
+    // função para realizar troca no heap
+    public static void swap(double[] vetor, int a, int b){
+        if(a < 0 || a >= vetor.length || b < 0 || b >= vetor.length){
+            throw new IllegalArgumentException("Indices inválidos para realizar swap");
         }
 
-        int i = Float.floatToIntBits(vetor[a]);
-        int j = Float.floatToIntBits(vetor[b]);
+        double aux = vetor[a];
+        vetor[a] = vetor[b];
+        vetor[b] = aux;
+    }
 
-        i = i ^ j;
-        j = i ^ j;
-        i = i ^ j;
+    public static int pai(int i){
+        int indicePai = (i - 1)/2;
+        return indicePai;
+    }
 
-        vetor[a] = Float.floatBitsToFloat(i);
-        vetor[b] = Float.floatBitsToFloat(j);
+    public static int filho_esquerda(int i){
+        int indiceFilhoEsquerda = (2 * i) + 1;
+        return indiceFilhoEsquerda;
+    }
 
-    }*/
+    public static int filho_direita(int i){
+        int indiceFilhoDireita = (2 * i) + 2;
+        return indiceFilhoDireita;
+    }
+
+    public static void heapify(double[] vetorOrdenar, int n, int indicePai){
+        int maior = indicePai;
+
+        int indiceFilhoEsquerda = Heap.filho_esquerda(indicePai);
+        int indiceFilhoDireita = Heap.filho_direita(indicePai);
+
+        if(indiceFilhoEsquerda < n && vetorOrdenar[maior] < vetorOrdenar[indiceFilhoEsquerda]){
+            maior = indiceFilhoEsquerda;
+        }
+
+        if(indiceFilhoDireita < n && vetorOrdenar[maior] < vetorOrdenar[indiceFilhoDireita]){
+            maior = indiceFilhoDireita;
+        }
+
+        if (maior != indicePai){
+            Heap.swap(vetorOrdenar, indicePai, maior);
+            Heap.heapify(vetorOrdenar, n, maior);
+        }
+    }
+
+    public static void buildHeap(double[] vetorOrdenar, int n){
+        int ultimoNo = n-1;
+        int indiceUltimoPai = Heap.pai(ultimoNo);
+
+        for(int i=indiceUltimoPai;i>=0;i--){
+            Heap.heapify(vetorOrdenar, n, i);
+        }
+    }
+
+    public static void heapSort(double[] vetorOrdenar, int n){
+        Heap.buildHeap(vetorOrdenar, n);
+
+        for(int i=n - 1;i>0;i--){
+            Heap.swap(vetorOrdenar, 0 , i);
+            Heap.heapify(vetorOrdenar, i , 0);
+        }
+    } 
+             
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args){
         // gerar dados para ordenação
-        int n = 3;
+        int n = 12;
         try{
             double[] vetorDados = DataGenerator.gerarDados(n, DataGenerator.INVERTIDO);
+            for(int i=0;i<n;i++){
+                if(i == 3){
+                    vetorDados[i] = 2;
+                }
+                if(i == 10){
+                    vetorDados[i] = 9;
+                }
+                System.out.println(vetorDados[i]);
+            }
+
+            Heap.heapSort(vetorDados, n);
+            System.out.println("Dados ordenados com sucesso: ");
             for(int i=0;i<n;i++){
                 System.out.println(vetorDados[i]);
             }
