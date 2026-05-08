957a5a1635e6998b1be9570000a90edeca30751f
Julya Gonçalves Pires <julyapires500@gmail.com>
2026-05-08 20:48:14 -0300

    Mudança no Smoothsort  a fim de melhorar a estrutura

--- Smoothsort.cpp
+++ Smoothsort.cpp
@@ -3,79 +3,93 @@
 
 using namespace std;
 
-void Smoothsort::organizar(vector<int>& valores, int raiz,int atual, int anterior){
+static const int LEO[] = {
+    1,1,3,5,9,15,25,41,67,109,177,287,465,753,1219,1973,
+    3193,5167,8361,13529,21891,35421,57313,92735,150049,
+    242785,392835,635621,1028457,1664079,2692537,4356617,
+    7049155,11405773,18454929,29860703,48315633,78176337,
+    126491971,204668309,331160281,535828591,866988873
+};
 
-    while(atual > 1){
+void Smoothsort::corrigirHeap(vector<int>& valores, int raiz, int ordem) {
+    while (ordem >= 2) {
+        int filhoDir = raiz - 1;
+        int filhoEsq = raiz - 1 - LEO[ordem - 2];
+        int maior = raiz;
 
-        int direita = raiz - 1;
-        int esquerda = raiz - 1 - anterior;
+        if (valores[filhoEsq] > valores[maior]) maior = filhoEsq;
+        if (valores[filhoDir] > valores[maior]) maior = filhoDir;
+        if (maior == raiz) break;
 
-        if(valores[raiz] >= valores[esquerda] &&
-           valores[raiz] >= valores[direita]){
-            break;
-        }
+        swap(valores[raiz], valores[maior]);
 
-        if(valores[esquerda] >= valores[direita]){
-
-            swap(valores[raiz], valores[esquerda]);
-
-            raiz = esquerda;
-
-            int aux = atual - anterior - 1;
-
-            atual = anterior;
-            anterior = aux;
-
-        }else{
-
-            swap(valores[raiz], valores[direita]);
-
-            raiz = direita;
-
-            atual = atual - 1;
-            anterior = anterior - 1;
-        }
+        if (maior == filhoEsq) { raiz = filhoEsq; ordem--; }
+        else                   { raiz = filhoDir; ordem -= 2; }
     }
 }
 
-void Smoothsort::reorganizar(vector<int>& valores, int raiz,int mascara, int atual, int anterior){
+void Smoothsort::inserirNaFloresta(vector<int>& valores, int raiz, int mascara, int ordem) {
+    while (mascara > 1) {
+        int vizinho = raiz - LEO[ordem];
 
-    while(mascara > 1){
+        if (valores[vizinho] <= valores[raiz]) break;
 
-        int esquerda = raiz - atual;
-
-        if(valores[esquerda] <= valores[raiz]){
-            break;
+        if (ordem >= 2) {
+            int filhoDir = raiz - 1;
+            int filhoEsq = raiz - 1 - LEO[ordem - 2];
+            if (valores[vizinho] <= valores[filhoDir] || valores[vizinho] <= valores[filhoEsq]) break;
         }
 
-        if(atual > 1){
-
-            int direita = raiz - 1;
-            int filhoEsq = raiz - 1 - anterior;
-
-            if(valores[esquerda] <= valores[filhoEsq] ||
-               valores[esquerda] <= valores[direita]){
-                break;
-            }
-        }
-
-        swap(valores[raiz], valores[esquerda]);
-
-        raiz = esquerda;
-
-        while(!(mascara & 1)){
-
-            mascara >>= 1;
-
-            int aux = atual + anterior + 1;
-
-            anterior = atual;
-            atual = aux;
-        }
+        swap(valores[raiz], valores[vizinho]);
+        raiz = vizinho;
 
         mascara >>= 1;
+        while (!(mascara & 1)) { mascara >>= 1; ordem++; }
+        ordem++;
     }
 
-    organizar(valores, raiz, atual, anterior);
+    corrigirHeap(valores, raiz, ordem);
 }
 
+void Smoothsort::sort(vector<int>& valores) {
+    int tamanho = valores.size();
+    if (tamanho < 2) return;
+
+    int mascara = 1;
+    int ordem = 1;
+
+    for (int i = 0; i < tamanho; i++) {
+        if ((mascara & 3) == 3) {
+            mascara = (mascara >> 2) | 1;
+            ordem += 2;
+        } else if (ordem == 1) {
+            mascara = (mascara << 1) | 1;
+            ordem = 0;
+        } else {
+            mascara = (mascara << (ordem - 1)) | 1;
+            ordem = 1;
+        }
+        inserirNaFloresta(valores, i, mascara, ordem);
+    }
+
+    for (int i = tamanho - 1; i > 0; i--) {
+        if (ordem <= 1) {
+            mascara &= ~1;
+            while (mascara && !(mascara & 1)) { mascara >>= 1; ordem++; }
+            if (mascara) mascara &= ~1;
+        } else {
+            int filhoDir = i - 1;
+            int filhoEsq = i - 1 - LEO[ordem - 2];
+            mascara &= ~1;
+
+            int novaMascara = (mascara << 1) | 1;
+            inserirNaFloresta(valores, filhoEsq, novaMascara, ordem - 1);
+
+            novaMascara = (novaMascara << 1) | 1;
+            inserirNaFloresta(valores, filhoDir, novaMascara, ordem - 2);
+
+            ordem -= 2;
+            mascara = (mascara << 2) | 3;
+        }
+    }
+}
