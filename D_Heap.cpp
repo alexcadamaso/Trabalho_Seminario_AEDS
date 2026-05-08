626f101c4047fe2e82457f8139173e56178cfaf5
Julya Gonçalves Pires <julyapires500@gmail.com>
2026-05-08 10:33:24 -0300

    Mudanças de detalhes nas funções

--- D_Heap.cpp
+++ D_Heap.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-Heap_d::Heap_d(bool isMax_Heap, int d){
+Heap_d::Heap_d(bool isMax_Heap,int d){
     this->isMax_Heap = isMax_Heap;
     this->d = d;
 }
@@ -12,21 +12,22 @@ void Heap_d::Heapify(vector<int> &valores3,int tamanho,int i){
     int filho, melhor = i;
 
     for(int k = 1; k <= d; k++){
-
       filho = (d * i) + k;
 
-      if(filho < tamanho && valores3[filho] > valores3[melhor]){
-         melhor = filho;
-      } 
-      
+      if(isMax_Heap){
+        if(filho < tamanho && valores3[filho] > valores3[melhor]){
+           melhor = filho;
+        }
+      } else {
+        if(filho < tamanho && valores3[filho] < valores3[melhor]){
+           melhor = filho;   
+        }
+      }
     }
-
     if(melhor != i){
          swap(valores3[melhor],valores3[i]);
          Heapify(valores3,tamanho,melhor);
-    }
-
-    
+    } 
 }
 
 void Heap_d::Build_Heap(vector<int> &valores3, int tamanho){
@@ -36,7 +37,7 @@ void Heap_d::Build_Heap(vector<int> &valores3, int tamanho){
 
 }
 
-void Heap_d::Heapsort2(vector<int> & valorse3,int tamanho){
+void Heap_d::Heapsort2(vector<int> & valores3,int tamanho){
     Build_Heap(valores3,tamanho);
 
     while(tamanho > 1){
