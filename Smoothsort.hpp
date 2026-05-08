3a5937231ac8c16a2b3b3a30a410f2b5e60b29f9
Julya Gonçalves Pires <julyapires500@gmail.com>
2026-05-08 20:48:14 -0300

    Mudança no Smoothsort  a fim de melhorar a estrutura

--- Smoothsort.hpp
+++ Smoothsort.hpp
@@ -1,14 +1,16 @@
 #ifndef SMOOTHSORT_HPP
 #define SMOOTHSORT_HPP
+#include <vector>
 
-class Smoothsort{
+class Smoothsort {
 
-private:    
-void organizar(vector<int>& valores,int raiz,int atual,int anterior);
-void reorganizar(vector<int>& valores,int raiz,int mascara,int atual,int anterior);
+private:
+    void corrigirHeap(std::vector<int>& valores, int raiz, int ordem);
+    void inserirNaFloresta(std::vector<int>& valores, int raiz, int mascara, int ordem);
 
+public:
+    void sort(std::vector<int>& valores);
 
 };
-
 
 #endif
