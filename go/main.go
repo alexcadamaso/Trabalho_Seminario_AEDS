bd1e7654657dd6744b31a32199721d70d93c7ef4
Ritaa-Marie <ritamariecajuru@gmail.com>
2026-05-08 17:15:12 -0300

    Função de leitura do arquivo de dados finalizada.

--- go/main.go
+++ go/main.go
@@ -1,12 +1,20 @@
 package main
 import "fmt"
+import "os"
 import "ordenacao_go/go/heap"
+import "ordenacao_go/go/data_manipulation"
 
 func main() {
-    n := 7
-    dados := []int{38, 27, 43, 3, 9, 82, 10}
+    n := 6
+    if n <= 0 || n > 1000000 {
+        fmt.Println("Número de entrada inválido, deve estar entre 1 e 1000000")
+        os.Exit(1)
+    }
+
+    caminho := "./data/invertido.dat"
+    dados := data_manipulation.LerDadosArquivo(caminho, n)
     fmt.Println("Dados: ", dados)
 
-    resultado := heap.HeapSortMax(dados, n)
+    resultado := heap.HeapSortMax(dados, len(dados))
     fmt.Println("Ordenado: ", resultado)
 }
