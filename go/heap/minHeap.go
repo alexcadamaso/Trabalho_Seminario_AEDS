654500fcb329f751eecf3d826d9657ed7e3cd139
Julya Gonçalves Pires <julyapires500@gmail.com>
2026-05-08 17:36:28 -0300

    Mudanças na estrutura

--- go/heap/minHeap.go
+++ go/heap/minHeap.go
@@ -1,45 +1,41 @@
-<<<<<<< HEAD
-package heap
-=======
 package heap
 
-func MinHeapify(valores []int,tamanho int,i int){
-	
-    esquerda := 2 * i + 1
-    direita := 2 * i + 2
-    melhor := i
+func MinHeapify(valores []int, tamanho int, i int) {
+
+	esquerda := 2*i + 1
+	direita := 2*i + 2
+	melhor := i
 
 	if esquerda < tamanho && valores[esquerda] < valores[melhor] {
-        melhor = esquerda
-    }
-    if direita < tamanho && valores[direita] < valores[melhor] {
-        melhor = direita
-    }
-    if melhor != i {
-        valores[melhor],valores[i] = valores[i],valores[melhor]
-        MinHeapify(valores,tamanho,melhor)
-    }
+		melhor = esquerda
+	}
+	if direita < tamanho && valores[direita] < valores[melhor] {
+		melhor = direita
+	}
+	if melhor != i {
+		valores[melhor], valores[i] = valores[i], valores[melhor]
+		MinHeapify(valores, tamanho, melhor)
+	}
 
 }
-func BuildMinHeap(valores []int,tamanho int){
-    for i := tamanho/2 - 1; i >= 0; i-- {
-        MinHeapify(valores,tamanho,i)
-    }
+func BuildMinHeap(valores []int, tamanho int) {
+	for i := tamanho/2 - 1; i >= 0; i-- {
+		MinHeapify(valores, tamanho, i)
+	}
 }
 
-func Heapsort(valores []int,tamanho int){
+func Heapsort(valores []int, tamanho int) {
 
-    BuildMinHeap(valores,tamanho)
+	BuildMinHeap(valores, tamanho)
 
-    for tamanho > 1 {
-    
-       ultimo := tamanho - 1
-      
-       valores[0],valores[ultimo] = valores[ultimo],valores[0]
-       tamanho--
-       MinHeapify(valores,tamanho,0)
-      
-    }
- 
+	for tamanho > 1 {
+
+		ultimo := tamanho - 1
+
+		valores[0], valores[ultimo] = valores[ultimo], valores[0]
+		tamanho--
+		MinHeapify(valores, tamanho, 0)
+
+	}
+
 }
->>>>>>> aed509d (implementa Heapsort(min) em Go)
