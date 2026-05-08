5f191d1673959cd16ad26d5facb7fda7114df83c
Julya Gonçalves Pires <julyapires500@gmail.com>
2026-05-08 10:34:19 -0300

    Main ainda para mudanças

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include "Heap.hpp"
+#include "D_Heap.hpp"
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -7,20 +8,31 @@
 using namespace std;
 
 int main(){
+    
 
+/*
 Max_Min Algorithm(true);
 Max_Min Algorithm2(false);
+Heap_d Algorithm3(true,4);
+Heap_d Algorithm4(false,4);
+
 vector<int> valores;
 vector<int> valores2;
+vector<int> valores3;
+vector<int> valores4;
+
 int tam;
 
-//srand(42);
+srand(42);
 
 for(int i = 0; i <= 15; i++){
-    //int num = rand() % 101;
-    //valores.push_back(num);
-    //valores2.push_back(num);
-    valores.push_back(i);
+    int num = rand() % 101;
+    valores.push_back(num);
+    valores2.push_back(num);
+    //valores.push_back(i);
+    //valores2.push_back(i);
+    valores3.push_back(i);
+    valores4.push_back(i);
 }
 
 tam = valores.size();
@@ -33,13 +45,44 @@ clock_t inicio2 = clock();
 Algorithm2.Heapsort(valores2,tam);
 clock_t fim2 = clock() - inicio2;
 
+clock_t inicio3 = clock();
+Algorithm3.Heapsort2(valores3,tam);
+clock_t fim3 = clock() - inicio3;
+
+clock_t inicio4 = clock();
+Algorithm4.Heapsort2(valores4,tam);
+clock_t fim4 = clock() - inicio4;
+
+
 
 cout << "Tempo gasto: " << (float)fim1/CLOCKS_PER_SEC << "\n";
 cout << "Tempo gasto: " << (float)fim2/CLOCKS_PER_SEC << "\n";
+cout << "Tempo gasto: " << (float)fim3/CLOCKS_PER_SEC << "\n";
+cout << "Tempo gasto: " << (float)fim4/CLOCKS_PER_SEC << "\n";
+
 cout << "Vetor ordenado: ";
 for(int i = 0; i < tam ; i++){
-    cout << valores[i] << " ";
+    cout << valores[i] << " "<< " ";
+    //cout << valores2[i] << " "<< "\t";
+    //cout << valores3[i] << " "<< "\n";
+    //cout << valores4[i] << " "<< "\n";
 }
+cout << "\n";
+for(int i = 0; i < tam ; i++){
+    cout << valores2[i] << " ";
+}
+cout << "\n";
 
+for(int i = 0; i < tam ; i++){
+    cout << valores3[i] << " "<< " ";
+    
+}
+cout << "\n";
+
+for(int i = 0; i < tam ; i++){
+    cout << valores4[i] << " "<< " ";
+}
+cout << "\n";
+*/
     return 0;
 }
