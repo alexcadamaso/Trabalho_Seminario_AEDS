76f40556d87b78c986a3e503973816267d49a87e
Julya Gonçalves Pires <julyapires500@gmail.com>
2026-05-08 17:42:47 -0300

    Arrumando

--- go/main.go
+++ go/main.go
@@ -1,25 +1,23 @@
-<<<<<<< HEAD
 package main
-import "fmt"
-import "os"
-import "ordenacao_go/go/heap"
-import "ordenacao_go/go/data_manipulation"
+
+import (
+	"fmt"
+	"ordenacao_go/go/data_manipulation"
+	"ordenacao_go/go/heap"
+	"os"
+)
 
 func main() {
-    n := 6
-    if n <= 0 || n > 1000000 {
-        fmt.Println("Número de entrada inválido, deve estar entre 1 e 1000000")
-        os.Exit(1)
-    }
+	n := 6
+	if n <= 0 || n > 1000000 {
+		fmt.Println("Número de entrada inválido, deve estar entre 1 e 1000000")
+		os.Exit(1)
+	}
 
-    caminho := "./data/invertido.dat"
-    dados := data_manipulation.LerDadosArquivo(caminho, n)
-    fmt.Println("Dados: ", dados)
+	caminho := "./data/invertido.dat"
+	dados := data_manipulation.LerDadosArquivo(caminho, n)
+	fmt.Println("Dados: ", dados)
 
-    resultado := heap.HeapSortMax(dados, len(dados))
-    fmt.Println("Ordenado: ", resultado)
+	resultado := heap.HeapSortMax(dados, len(dados))
+	fmt.Println("Ordenado: ", resultado)
 }
-=======
-
-
->>>>>>> aed509d (implementa Heapsort(min) em Go)
