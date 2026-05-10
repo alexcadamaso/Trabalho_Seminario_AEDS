0eadeb6244128adeb1d35ed07d7444ec82e54f79
Mariana Kaori Yano <166726545+marisxzh@users.noreply.github.com>
2026-05-10 18:24:27 -0300

    atualização do main
    Adaptação para ter um arquivo txt (results.txt) com os resultados dos métodos (tempo e memória utilizada)

--- main.c
+++ main.c
@@ -8,50 +8,111 @@
 #include "include/heap_d.h"
 #include "include/smoothsort.h"
 
+#include <sys/resource.h> // getrusage
 
-void executar(int lista_original[], int n) {
+long get_mem_usage() {
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    // retorna em kilobytes (KB)
+    return usage.ru_maxrss; 
+}
+
+
+void executar(int lista_original[], int n, FILE *fp) {
     // array temporário para as cópias
     int *lista_teste = (int*)malloc(n * sizeof(int));
     clock_t inicio, fim;
     double tempo;
+    long mem_antes, mem_depois, mem_usada;
 
-    printf("\n--- RESULTADOS DOS TESTES (N = %d) ---\n", n);
-    printf("%-20s | %-15s\n", "Algoritmo", "Tempo (s)");
-    printf("------------------------------------------\n");
+    printf("\n       RESULTADOS (N = %d)       \n\n", n);
+    printf("%-20s | %-15s| %-15s\n", "Algoritmo", "Tempo (s)", "Memória (kb)");
+    printf("-----------------------------------------------------------\n");
 
-    // --- TESTE 1: HeapSort Max ---
+
+    fprintf(fp, "%-20s | %-15s| %-15s\n", "Algoritmo", "Tempo (s)", "Memória (kb)");
+
+
+    //HeapSort Max
+    
+    mem_antes = get_mem_usage();
     copiar_lista(lista_teste, lista_original, n);
+
+    
     inicio = clock();
+    
     heapSortMax(lista_teste, n);
+    mem_depois = get_mem_usage();
+    
     fim = clock();
-    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("%-20s | %-15.6f\n", "HeapSort Max", tempo);
+    mem_usada = mem_depois - mem_antes;
 
-    // --- TESTE 2: HeapSort Min ---
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("%-20s | %-15f | %-15ld\n", "HeapSort Max", tempo, mem_usada);
+    fprintf(fp, "%-20s | %-15f| %-15ld\n", "HeapSort Max", tempo, mem_usada);
+
+
+
+    //HeapSort Min
+    
+    mem_antes = get_mem_usage();
     copiar_lista(lista_teste, lista_original, n);
+    
     inicio = clock();
+    
     heapSortMin(lista_teste, n);
+    
     fim = clock();
+    mem_depois = get_mem_usage();
+    
     tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("%-20s | %-15.6f\n", "HeapSort Min", tempo);
+    mem_usada = mem_depois - mem_antes;
 
-    // --- TESTE 3: Heap D-ário (D=3) ---
+    printf("%-20s | %-15f | %-15ld\n", "HeapSort Min", tempo, mem_usada);
+    fprintf(fp, "%-20s | %-15f| %-15ld\n", "HeapSort Min", tempo, mem_usada);
+
+
+
+
+    //Heap D-ário (D=3)
+    
+    mem_antes = get_mem_usage();
     copiar_lista(lista_teste, lista_original, n);
+    
     inicio = clock();
+    
     heapSortD(lista_teste, n, 3); 
+    
     fim = clock();
+    mem_depois = get_mem_usage();
+    
     tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("%-20s | %-15.6f\n", "Heap D-ario (D=3)", tempo);
+    mem_usada = mem_depois - mem_antes;
 
-    // --- TESTE 4: Smoothsort ---
+    printf("%-20s | %-15f | %-15ld\n", "Heap D-ario (D=3)", tempo, mem_usada);
+    fprintf(fp, "%-20s | %-15f| %-15ld\n", "Heap D-ario (D=3)", tempo, mem_usada);
+
+
+
+    
+    //Smoothsort
+    mem_antes = get_mem_usage();
     copiar_lista(lista_teste, lista_original, n);
+    
     inicio = clock();
+    
     smoothSort(lista_teste, n);
+    
     fim = clock();
+    mem_depois = get_mem_usage();
+    
     tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("%-20s | %-15.6f\n", "Smoothsort", tempo);
+    mem_usada = mem_depois - mem_antes;
 
-    printf("------------------------------------------\n");
+    printf("%-20s | %-15f | %-15ld\n", "Smoothsort", tempo, mem_usada);
+    fprintf(fp, "%-20s | %-15f| %-15ld\n", "Smoothsort", tempo, mem_usada);
+
+    printf("-----------------------------------------------------------\n");
 
     free(lista_teste);
 }
@@ -60,8 +121,20 @@ void executar(int lista_original[], int n) {
 int main() {
 
     int n; 
+
+    FILE *fp = fopen("results.txt", "w");
+
+    if (fp == NULL) {
+        printf("Erro ao criar o arquivo de resultados!\n");
+        return 1;
+    }
+
+
+    fprintf(fp, "Resultados\n\n");
     
-    printf("Números aleatórios\n");
+    printf("\n\nNúmeros aleatórios\n");
+    fprintf(fp, "Cenário: Números Aleatórios\n"); 
+    
 
     int* listaA = ler_arquivo("aleatorio.txt", &n);
 
@@ -70,12 +143,13 @@ int main() {
         return 1;
     }
 
-    executar(listaA, n);
+    executar(listaA, n, fp);
     free(listaA);
 
 
 
-    printf("Números invertidos\n");
+    printf("\n\nNúmeros invertidos\n");
+    fprintf(fp, "\nCenário: Números Invertidos\n");
 
     int* listaI = ler_arquivo("invertido.txt", &n);
 
@@ -84,11 +158,12 @@ int main() {
         return 1;
     }
 
-    executar(listaI, n);
+    executar(listaI, n, fp);
     free(listaI);
 
 
-    printf("Números ordenados\n");
+    printf("\n\nNúmeros ordenados\n");
+    fprintf(fp, "\nCenário: Números Ordenados\n");
 
 
     int* listaO = ler_arquivo("ordenado.txt", &n);
@@ -98,8 +173,13 @@ int main() {
         return 1;
     }
 
-    executar(listaO, n);
+    executar(listaO, n, fp);
     free(listaO);
+
+
+    fclose(fp);
+
+
     return 0;
 }
 
