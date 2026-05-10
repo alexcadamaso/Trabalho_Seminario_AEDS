2f33f318d28fab9bc6487805f3c524e721ba1ffe
Julya Gonçalves Pires <julyapires500@gmail.com>
2026-05-10 19:38:19 -0300

    Mudanças finais

--- include/GerenciadorArquivos.hpp
+++ include/GerenciadorArquivos.hpp
@@ -1,33 +1,26 @@
-#pragma once
+#ifndef GERENCIADOR_ARQUIVOS_HPP
+#define GERENCIADOR_ARQUIVOS_HPP
 
-#include "D_Heap.hpp"
-#include "Smoothsort.hpp"
-#include <string>
 #include <vector>
+#include <string>
 #include <fstream>
 
-using namespace std;
-
 class GerenciadorArquivos {
-
-    public:
-
-    GerenciadorArquivos(int repeticoes,string caminhoLog,string caminhoResumo);
+public:
+    GerenciadorArquivos(int repeticoes, std::string caminhoLog, std::string caminhoResumo);
     ~GerenciadorArquivos();
-    void rodar(vector<string> tipos,vector<string> algoritmos,vector<int> valoresD,string pastaData);
+    void rodar(std::vector<int> tamanhos, std::vector<std::string> tipos, std::vector<std::string> algoritmos, std::vector<int> valoresD, std::string pastaData);
 
-    private:
-
+private:
     int repeticoes;
+    std::ofstream outLog;
+    std::ofstream outResumo;
 
-    ofstream outLog;
-    ofstream outResumo;
-
-    void executarUm(string algo,vector<int>& vetor,bool isMax,int d,double& tempo,long& memoria);
-    void rodarBloco(vector<int>& original,string tipo,string algo,int d);
-    vector<int> lerArquivo(string caminho);
-    void gravarLinha(int indice,string algo,int d,string tipo,double tempo,long memoria);
-    void gravarResumo(string algo,int d,string tipo,double tempoMedio, long memoriaMedia);
-
-
+    void executarUm(std::string algo, std::vector<int>& vetor, bool isMax, int d, double& tempo, long& memoria);
+    void rodarBloco(std::vector<int>& original, std::string tipo, std::string algo, int d);
+    std::vector<int> lerArquivo(std::string caminho, int n);
+    void gravarLinha(int indice, std::string algo, int d, int tamanho, std::string tipo, double tempo, long memoria);
+    void gravarResumo(std::string algo, int d, int tamanho, std::string tipo, double tempoMedio, long memoriaMedia);
 };
+
+#endif
